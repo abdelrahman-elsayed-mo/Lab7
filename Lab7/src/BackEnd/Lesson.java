c19cb1b52b13ad46b2845a2542ae90c56c1d194f
Youssef Fathy <yousseffathy771@gmail.com>
2025-11-16 21:31:15 +0200

    added class Lesson

--- Lab7/src/BackEnd/Lesson.java
+++ Lab7/src/BackEnd/Lesson.java
@@ -5,6 +5,49 @@
 package BackEnd;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Lesson {
-    
+
+    private String lessonId;
+    private String title;
+    private String content;
+    private List<String> resources;
+
+    public Lesson(String title, String content) {
+        this.lessonId = UUID.randomUUID().toString();
+        this.title = title;
+        this.content = content;
+        this.resources = new ArrayList<>();
+    }
+
+    public String getLessonId() {
+        return lessonId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getResources() {
+        return resources;
+    }
+
+    public void addResource(String resource) {
+        this.resources.add(resource);
+    }
 }
