5f3785281f2b9272d3f0c5fe5a248cb7bb942025
Youssef Fathy <yousseffathy771@gmail.com>
2025-11-16 21:30:56 +0200

    added class Instructor

--- Lab7/src/BackEnd/Instructor.java
+++ Lab7/src/BackEnd/Instructor.java
@@ -5,6 +5,23 @@
 package BackEnd;
 
 
-public class Instructor {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Instructor extends User {
+
+    private List<String> createdCourses;
+
+    public Instructor(String userId, String username, String email, String passwordHash) {
+        super(userId, "Instructor", username, email, passwordHash);
+        this.createdCourses = new ArrayList<>();
+    }
+
+    public List<String> getCreatedCourses() {
+        return createdCourses;
+    }
+
+    public void setCreatedCourses(List<String> createdCourses) {
+        this.createdCourses = createdCourses;
+    }
 }
