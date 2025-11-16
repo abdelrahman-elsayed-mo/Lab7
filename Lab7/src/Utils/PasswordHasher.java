6b7201f56f4a3226604a42f7bde89d508e33717b
Youssef Fathy <yousseffathy771@gmail.com>
2025-11-16 21:34:27 +0200

    added PasswordHasher

--- Lab7/src/Utils/PasswordHasher.java
+++ Lab7/src/Utils/PasswordHasher.java
@@ -5,6 +5,27 @@
 package Utils;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
+
 public class PasswordHasher {
-    
+
+    public static String hash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            BigInteger number = new BigInteger(1, hash);
+            StringBuilder hexString = new StringBuilder(number.toString(16));
+
+            while (hexString.length() < 64) {
+                hexString.insert(0, '0');
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
