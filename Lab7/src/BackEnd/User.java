2b406263bbc13e84325510ec7de97cf5eeea8c6c
Youssef Fathy <yousseffathy771@gmail.com>
2025-11-16 21:31:53 +0200

    added class User

--- Lab7/src/BackEnd/User.java
+++ Lab7/src/BackEnd/User.java
@@ -4,7 +4,39 @@
  */
 package BackEnd;
 
+public abstract class User {
 
-public class User {
-    
-}
+    private String userId;
+    private String role;
+    private String username;
+    private String email;
+    private String passwordHash;
+
+    public User(String userId, String role, String username, String email, String passwordHash) {
+        this.userId = userId;
+        this.role = role;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+} 
