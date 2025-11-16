b9bdd0e98a99cac83d84bed5d22a0f691ed18b12
AdamAyman <adamaymna7812@gmail.com>
2025-11-16 20:15:03 +0200

    Merge origin/main into main
    # Conflicts:
    #	lab4/src/lab4/Lab4.java

--- Lab7/src/Services/InstructorService.java
+++ Lab7/src/Services/InstructorService.java
@@ -61,6 +61,93 @@ public class InstructorService extends JFrame {
 
         add(tabbedPane);
     }
+    
+      private JPanel createCoursesPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        String[] columns = {"ID", "Title", "Description", "Students", "Lessons"};
+        coursesTableModel = new DefaultTableModel(columns, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        coursesTable = new JTable(coursesTableModel);
+        JScrollPane scrollPane = new JScrollPane(coursesTable);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        createCourseButton = new JButton("Create Course");
+        editCourseButton = new JButton("Edit Course");
+        deleteCourseButton = new JButton("Delete Course");
+        viewStudentsButton = new JButton("View Enrolled Students");
+
+        buttonPanel.add(createCourseButton);
+        buttonPanel.add(editCourseButton);
+        buttonPanel.add(deleteCourseButton);
+        buttonPanel.add(viewStudentsButton);
+
+        createCourseButton.addActionListener(e -> createCourse());
+        editCourseButton.addActionListener(e -> editCourse());
+        deleteCourseButton.addActionListener(e -> deleteCourse());
+        viewStudentsButton.addActionListener(e -> viewEnrolledStudents());
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        return panel;
+    }
+
+    private JPanel createLessonsPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        String[] columns = {"ID", "Title", "Course", "Order", "Content Preview"};
+        lessonsTableModel = new DefaultTableModel(columns, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        lessonsTable = new JTable(lessonsTableModel);
+        JScrollPane scrollPane = new JScrollPane(lessonsTable);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        addLessonButton = new JButton("Add Lesson");
+        editLessonButton = new JButton("Edit Lesson");
+        deleteLessonButton = new JButton("Delete Lesson");
+
+        buttonPanel.add(addLessonButton);
+        buttonPanel.add(editLessonButton);
+        buttonPanel.add(deleteLessonButton);
+
+        addLessonButton.addActionListener(e -> addLesson());
+        editLessonButton.addActionListener(e -> editLesson());
+        deleteLessonButton.addActionListener(e -> deleteLesson());
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        return panel;
+    }
+
+    private JPanel createStudentsPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        String[] columns = {"Student ID", "Name", "Email", "Course"};
+        studentsTableModel = new DefaultTableModel(columns, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        studentsTable = new JTable(studentsTableModel);
+        JScrollPane scrollPane = new JScrollPane(studentsTable);
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+        return panel;
+    }
+
+
+    
 
    
 }
